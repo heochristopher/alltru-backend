08f2b53decedfbb957325f652de18646c86fac54
heochristopher <71462915+heochristopher@users.noreply.github.com>
2022-07-20 17:43:12 -0400

    logouit

--- dist/middleware/services/logout.js
+++ dist/middleware/services/logout.js
@@ -15,7 +15,12 @@ const logout = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
         const token = req.cookies['auth-token'];
         if (!token)
             return res.status(400).json('You are not signed in');
-        res.clearCookie('auth-token').status(200).json('You have successfully logged out');
+        res.clearCookie('auth-token', {
+            secure: true,
+            sameSite: 'strict',
+            httpOnly: true,
+            domain: '.alltru.app'
+        }).status(200).json('You have successfully logged out');
     }
     catch (error) {
         res.status(400).json(error);
--- src/middleware/services/logout.ts
+++ src/middleware/services/logout.ts
@@ -4,7 +4,12 @@ export const logout = async (req: Request, res: Response) => {
     try {
         const token = req.cookies['auth-token']
         if(!token) return res.status(400).json('You are not signed in')
-        res.clearCookie('auth-token').status(200).json('You have successfully logged out')
+        res.clearCookie('auth-token', {
+            secure: true,
+            sameSite: 'strict',
+            httpOnly: true,
+            domain: '.alltru.app'
+        }).status(200).json('You have successfully logged out')
     } catch (error) {
         res.status(400).json(error)
     }
